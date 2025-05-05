db0acb5dda5463477f1a0b27907f65646a12cd0b
5jantuan <anastasiajant10@gmail.com>
2025-05-06 02:30:07 +0300

    layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -45,7 +45,7 @@
     </div>
     <div class="navbar navbar-dark bg-dark shadow-sm">
         <div class="container">
-        <a href="/home" class="navbar-brand d-flex align-items-center">
+        <a href="/" class="navbar-brand d-flex align-items-center">
             <strong>EmptySun</strong>
         </a>
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
