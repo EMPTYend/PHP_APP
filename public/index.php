5a88ed42ac91a8c7ed8433e15afe199ee4a12e69
EMPTYend <vip.iarik@inbox.ru>
2025-05-05 23:54:41 +0300

    Back

--- public/index.php
+++ public/index.php
@@ -5,15 +5,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/Core/Database.php';
 require_once __DIR__ . '/../app/Core/Router.php';
 require_once __DIR__ . '/../app/Controllers/RoomController.php';
-require_once __DIR__ . '/../app/Models/User.php';
 require_once __DIR__ . '/../app/Core/View.php';
 
 
 // Получаем подключение к БД
 $pdo = \app\Core\Database::connect();
-
-// Инициализация моделей (исправлен namespace)
-$userModel = new \app\Models\User($pdo);
 
 // Создание маршрутизатора
 $router = new \app\Core\Router();
--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,3 @@ $router->get('/', ['app\Controllers\RoomController', 'index']);
 $router->get('/home', ['app\Controllers\RoomController', 'home']);
 $router->get('/rooms', ['app\Controllers\RoomController', 'rooms']);
 $router->post('/room/search', ['app\Controllers\RoomController', 'search']);
-
-// Маршруты аутентификации
-$router->get('/login', ['app\Controllers\AuthController', 'login']);
-$router->post('/login', ['app\Controllers\AuthController', 'authenticate']);
-$router->get('/logout', ['app\Controllers\AuthController', 'logout']);
-
-// Маршруты аккаунта
-$router->get('/account', ['app\Controllers\AccountController', 'index']);
-$router->get('/account/profile', ['app\Controllers\AccountController', 'profile']);
-$router->post('/account/update-profile', ['app\Controllers\AccountController', 'updateProfile']);
-$router->get('/account/bookings', ['app\Controllers\AccountController', 'bookings']);
-$router->get('/account/security', ['app\Controllers\AccountController', 'security']);
-$router->post('/account/change-password', ['app\Controllers\AccountController', 'changePassword']);
