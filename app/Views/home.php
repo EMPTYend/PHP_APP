c7c8d0a9a1d110382b29383c5df5d7e47505afa8
5jantuan <anastasiajant10@gmail.com>
2025-05-06 03:50:23 +0300

    add search

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,8 +6,11 @@
         <h1 class="fw-light">Album example</h1>
         <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
         <p>
-          <a href="#" class="btn btn-primary my-2">Main call to action</a>
-          <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+          <a href="#" class="btn btn-primary my-2">Contact us</a>
+          <form class="d-flex" role="search"> 
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> 
+            <button class="btn btn-outline-success" type="submit">Search</button> 
+          </form>
         </p>
       </div>
     </div>
