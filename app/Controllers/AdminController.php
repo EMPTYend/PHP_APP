2cabf22ad018eb450608e09a684e2d57c3d62b87
EMPTYend <vip.iarik@inbox.ru>
2025-05-06 11:12:51 +0300

    -

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -6,7 +6,6 @@ use app\Core\Controller;
 use app\Models\Room;
 use app\Core\View;
 use app\Models\User;
-use app\Models\Room;
 use app\Core\Middleware\AuthMiddleware;
 
 class AdminController extends Controller
